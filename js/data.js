f6952e5f7f6bc102023f8c2ee829aad04cd76ef9
adityabajra <adityabajracharya18@gmail.com>
2026-02-03 21:40:38 +0545

    added data file

---
+++ js/data.js
@@ -0,0 +1,6 @@
+export const groceryItems = [
+  { id: "1", name: "ham", completed: true },
+  { id: "2", name: "cheese", completed: true },
+  { id: "3", name: "eggs", completed: false },
+  { id: "4", name: "apple", completed: false },
+];
