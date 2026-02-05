71e750e0ffbfcfa209149c80370720af701df288
adityabajra <adityabajracharya18@gmail.com>
2026-02-05 18:57:45 +0545

    added form

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 import { groceryItems } from "./data.js";
 import { createItems } from "./items.js";
+import { createForm } from "./form.js";
 
 let items = groceryItems;
 
 function render() {
   const app = document.getElementById("app");
   app.innerHTML = "";
-
+  const formElement = createForm();
   const itemsElement = createItems(items);
+  app.appendChild(formElement);
   app.appendChild(itemsElement);
 }
 
 render();
+function generateId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
 export function editCompleted(itemId) {
   items = items.map((item) => {
     if (item.id === itemId) {
@@ -20,6 +25,16 @@ export function editCompleted(itemId) {
     return item;
   });
   render();
+}
+export function addItem(itemName) {
+  const newItem = {
+    name: itemName,
+    completed: false,
+    id: generateId(),
+  };
+  items = [...items, newItem];
+  render();
+  setTimeout(() => alert("Item Added Successfully!"), 0);
 }
 export function removeItem(itemId) {
   items = items.filter((item) => item.id !== itemId);
